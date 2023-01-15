e0b98f7e102c4bb402067361da239623f62e331b
dimorinny <didika914@gmail.com>
2023-01-15 13:24:57 +0000

    Implement basic move/cast/target and implement very basic bot

--- groundhog/__main__.py
+++ groundhog/__main__.py
@@ -2,6 +2,25 @@ import time
 
 from .character import Character
 
+def cast_around(character: Character):
+    for _ in range(10):
+        character.target()
+        character.cast('3')
+        character.target()
+        character.cast('3')
+        character.target()
+        character.cast('3')
+        character.target()
+        character.cast('3')
+        character.turn(45)
+
+def loop(character: Character):
+    while True:
+        cast_around(character=character)
+        character.move(8)
+
+# TODO: detect game is opened to start playing
 time.sleep(2)
+
 character = Character()
-character.turn(90)
+loop(character=character)
--- groundhog/character.py
+++ groundhog/character.py
@@ -1,3 +1,5 @@
+import time
+
 import pyautogui as gui
 
 class Character:
@@ -8,6 +10,7 @@ class Character:
     def __init__(self) -> None:
         self._screen_width, self._screen_height = gui.size()
 
+    # TODO: consider turning using /script FlipCameraYaw(180) instead
     def turn(self, degree: int) -> None:
         degree = degree % 360
         if degree < 0:
@@ -19,10 +22,29 @@ class Character:
         self._move_mouse_to_center(x=0)
         self._drag(x_offset=pixels_to_turn, y_offset=0, button=gui.RIGHT)
 
+    # TODO: async?
+    # Holding 2 mouse buttons move your character towards current direction
+    def move(self, seconds: int) -> None:
+        self._move_mouse_to_center(x=0)
+        gui.mouseDown(button=gui.LEFT)
+        gui.mouseDown(button=gui.RIGHT)
+        time.sleep(seconds)
+        gui.mouseUp(button=gui.LEFT)
+        gui.mouseUp(button=gui.RIGHT)
+
+    def cast(self, keybind: str):
+        gui.press(keybind)
+        # TODO: detect cast bar using CV and don't use hardcoded value
+        time.sleep(2)
+
+    # TODO: detect target selected using CV and return bool to identify it
+    def target(self):
+        gui.press('tab')
+
     def _drag(self, x_offset: int, y_offset: int, button: str) -> None:
-        gui.mouseDown(button=button, logScreenshot=None)
+        gui.mouseDown(button=button)
         gui.moveRel(xOffset=x_offset, yOffset=y_offset)
-        gui.mouseUp(button=button, logScreenshot=None)
+        gui.mouseUp(button=button)
 
     def _move_mouse_to_center(self, x=None, y=None) -> None:
         final_x = x if x is not None else self._screen_width / 2
